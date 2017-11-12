a33fe9f49881cffe3e74a224567f457b7cbcb803
edunzerlaptop <ethandunzer@yahoo.com>
2017-11-11 20:29:08 -0800

    new proccessing program from the coding train. Next step is to make the overal size adjustable

---
+++ Proccessing/Clock/clock.js
@@ -0,0 +1,55 @@
+//window setup
+ function setup() {
+   createCanvas(400, 400);
+   angleMode(DEGREES);
+ }
+ //main clock
+ function draw() {
+   background(0);
+   translate(200, 200);
+   rotate(-90);
+
+   let hr = hour();
+   let mn = minute();
+   let sc = second();
+
+   strokeWeight(8);
+   stroke(255, 100, 150);
+   noFill();
+   //SECOND hand
+   let secondAngle = map(sc, 0, 60, 0, 360);
+   //clock circle
+   arc(0, 0, 300, 300, 0, secondAngle);
+   //MINUTE hand
+   stroke(150, 100, 255);
+   let minuteAngle = map(mn, 0, 60, 0, 360);
+   //clock circle
+   arc(0, 0, 280, 280, 0, minuteAngle);
+   //HOUR hand
+   stroke(150, 255, 100);
+   let hourAngle = map(hr % 12, 0, 12, 0, 360);
+   //clock circle
+   arc(0, 0, 260, 260, 0, hourAngle);
+
+   push();
+   rotate(secondAngle);
+   stroke(255, 100, 150);
+   line(0, 0, 100, 0);
+   pop();
+
+   push();
+   rotate(minuteAngle);
+   stroke(150, 100, 255);
+   line(0, 0, 75, 0);
+   pop();
+
+   push();
+   rotate(hourAngle);
+   stroke(150, 255, 100);
+   line(0, 0, 50, 0);
+   pop();
+
+   stroke(255);
+   point(0, 0);
+
+ }
