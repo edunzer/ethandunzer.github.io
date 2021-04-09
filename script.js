821a425305b4cfe67199d8e9f913780344d091ed
Ethan Dunzer <ethandunzer@yahoo.com>
2021-04-08 21:52:03 -0700

    helper navigation update
    added bouncing arrow for navigation

--- script.js
+++ script.js
@@ -17,6 +17,43 @@ $( document ).ready(function() {
     'top': '150%'
   },1000);
 
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({
+    behavior: 'smooth'
+  });
+
+  // move section down one
+  $(document).on("click", "#moveDown", function () {
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  // smooth scrolling
+  $(document).ready(function(){
+  // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+    });
+
+
 
   $(window).scroll( function(){
 
